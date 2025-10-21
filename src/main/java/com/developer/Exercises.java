c6ae79c8c14bea5f5402cbb70c72df87521c798b
Ronald Espitia <ronald79@gmail.com>
2025-10-21 14:55:38 -0500

    Ejercicios

---
+++ src/main/java/com/developer/Exercises.java
@@ -0,0 +1,60 @@
+package com.developer;
+
+public class Exercises {
+    /*
+1. Operaciones básicas
+
+Objetivo: Practicar operadores aritméticos y tipos primitivos.
+
+Instrucciones:
+Declara tres variables de tipo int llamadas a, b y c.
+
+Asigna valores a a y b.
+
+Calcula la suma, resta, multiplicación, división y módulo de a y b.
+
+Guarda cada resultado en una variable diferente.
+
+Muestra los resultados por consola con mensajes descriptivos.
+
+
+2. Comparaciones numéricas
+
+Objetivo: Practicar operadores relacionales.
+
+Instrucciones:
+Declara dos variables double llamadas x y y.
+
+Asigna valores diferentes.
+
+Imprime el resultado de comparar si:
+
+x es mayor que y
+
+x es menor o igual que y
+
+x es igual a y
+
+x es diferente de y
+
+
+3. Operadores lógicos
+
+Objetivo: Practicar &&, || y !.
+
+Instrucciones:
+Declara tres variables booleanas: tieneEdadValida, tieneDocumento, esResidente.
+
+Asigna valores true o false.
+
+Crea expresiones lógicas como:
+
+puedeEntrar = tieneEdadValida && tieneDocumento
+
+puedeSolicitar = tieneDocumento || esResidente
+
+noPuedeEntrar = !puedeEntrar
+
+Imprime los valores de cada expresión.
+     * */
+}
